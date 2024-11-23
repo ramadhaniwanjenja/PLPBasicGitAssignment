08d22a6ad0bd9d427318fb2bbf3738768db1d385
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-11-23 19:02:44 +0200

    frontend

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './pages/Home';
+import reactLogo from './pages/Home'
+
+function App() {
+
+  return (
+    <Home></Home>
+  );
+}
+
+export default App
---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,59 @@
+function Home(){
+    return(
+<div className="Home" style={{width: 1440, height: 1024, position: 'relative', background: '#334640', borderRadius: 40, overflow: 'hidden'}}>
+  <div className="Ellipse2380" style={{width: 503, height: 415, left: 1067, top: -126, position: 'absolute', background: 'linear-gradient(180deg, #419AE8 0%, white 100%)', boxShadow: '200px 200px 200px ', borderRadius: 9999, filter: 'blur(200px)'}} />
+  <div className="Ellipse2380" style={{width: 503, height: 415, left: -126, top: 774, position: 'absolute', background: 'linear-gradient(180deg, #419AE8 0%, white 100%)', boxShadow: '200px 200px 200px ', borderRadius: 9999, filter: 'blur(200px)'}} />
+  <div className="UserProfile" style={{width: 319, height: 37, left: 574, top: 28, position: 'absolute', color: 'white', fontSize: 40, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word'}}>User Profile</div>
+  <div className="ManageYourDetailsAndChangeYourPassword" style={{left: 444, top: 116, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Manage your details and change your password</div>
+  <div className="Frame57" style={{width: 264, height: 332, left: 125, top: 189.24, position: 'absolute', borderRadius: 20, overflow: 'hidden', border: '1px white solid'}}>
+    <div className="AnnaBella" style={{left: 66, top: 195, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Anna Bella</div>
+    <div className="250749520624" style={{left: 66, top: 229, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>+250 749 520 624</div>
+    <img className="Ellipse4" style={{width: 100, height: 100, left: 82, top: 42.76, position: 'absolute', background: 'linear-gradient(0deg, #F8F7F7 0%, #F8F7F7 100%)', borderRadius: 9999}} src="https://via.placeholder.com/100x100" />
+  </div>
+  <div className="Frame58" style={{width: 674, height: 332, left: 471, top: 189.24, position: 'absolute', borderRadius: 20, overflow: 'hidden', border: '1px white solid'}}>
+    <div className="GeneralInformation" style={{left: 43, top: 28, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>General information</div>
+    <div className="Frame59" style={{width: 240, height: 59, left: 36, top: 85, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid'}}>
+      <div className="FullName" style={{left: 19, top: 6, position: 'absolute'}}><span style="color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'">full</span><span style="color: 'white', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'"> </span><span style="color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'">name</span></div>
+      <div className="AnnBella" style={{left: 17, top: 26, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Ann Bella</div>
+    </div>
+    <div className="Frame60" style={{height: 59, paddingTop: 7, paddingBottom: 16, paddingLeft: 16, paddingRight: 167, left: 306, top: 85, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 6, display: 'inline-flex'}}>
+      <div className="Gender" style={{color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Gender</div>
+      <div className="Female" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Female</div>
+    </div>
+    <div className="Frame61" style={{height: 59, paddingTop: 5, paddingBottom: 15, paddingLeft: 16, paddingRight: 169, left: 34, top: 183, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 9, display: 'inline-flex'}}>
+      <div className="Nationality" style={{color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Nationality</div>
+      <div className="Nigeria" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Nigeria</div>
+    </div>
+    <div className="Frame62" style={{paddingTop: 16, paddingBottom: 16, paddingLeft: 27, paddingRight: 26, left: 417, top: 188, position: 'absolute', background: '#0EC300', borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div className="Update" style={{color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '500', wordWrap: 'break-word'}}>Update</div>
+    </div>
+  </div>
+  <div className="Frame59" style={{width: 674, height: 332, left: 471, top: 570.24, position: 'absolute', borderRadius: 20, overflow: 'hidden', border: '1px white solid'}}>
+    <div className="Security" style={{left: 43, top: 28, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Security</div>
+    <div className="Frame59" style={{width: 240, height: 59, left: 34, top: 85, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid'}}>
+      <div className="Email" style={{left: 19, top: 6, position: 'absolute', color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
+      <div className="ExampleGmailCom" style={{left: 11, top: 30, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>example@gmail.com</div>
+    </div>
+    <div className="Frame60" style={{paddingTop: 7, paddingBottom: 11, paddingLeft: 16, paddingRight: 95, left: 306, top: 85, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 11, display: 'inline-flex'}}>
+      <div className="PhoneNumber" style={{color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>phone number</div>
+      <div className="250749520624" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>+250 749 520 624</div>
+    </div>
+    <div className="Frame61" style={{width: 240, height: 59, left: 34, top: 183, position: 'absolute', borderRadius: 10, overflow: 'hidden', border: '1px white solid'}}>
+      <div className="Password" style={{left: 16, top: 5, position: 'absolute', color: '#8C8C8C', fontSize: 10, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
+      <div style={{left: 13, top: 23, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>...............</div>
+    </div>
+    <div className="Frame62" style={{paddingTop: 15, paddingBottom: 15, paddingLeft: 16, paddingRight: 23, left: 63, top: 268, position: 'absolute', background: '#0EC300', borderRadius: 20, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+      <div className="ChangePassword" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>change password</div>
+    </div>
+    <div className="Frame63" style={{paddingLeft: 9, paddingRight: 9, paddingTop: 15, paddingBottom: 15, left: 264, top: 268, position: 'absolute', background: '#0EC300', borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div className="ChangePhoneNumber" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>change phone number</div>
+    </div>
+  </div>
+  <div className="Frame62" style={{paddingTop: 15, paddingBottom: 15, paddingLeft: 40, paddingRight: 39, left: 174, top: 829.24, position: 'absolute', background: '#E33629', borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div className="SignOut" style={{color: 'white', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '500', wordWrap: 'break-word'}}>sign out</div>
+  </div>
+</div>
+    );
+}
+
+export default Home
